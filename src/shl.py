98538265e1535e6195eb3897cc64c839964a2eca
Gary Benson <gary@gbenson.net>
2023-04-05 00:27:30 +0100

    Don't post nothing

--- src/shl.py
+++ src/shl.py
@@ -8,6 +8,9 @@ from imaplib import IMAP4_SSL
 
 
 def main():
+    payload = " ".join(sys.argv[1:]).strip()
+    assert payload, "usage: shl URL [TEXT...]"
+
     filename = os.path.expanduser("~/.shlrc")
     with open(filename) as fp:
         config = json.load(fp)
@@ -15,7 +18,7 @@ def main():
     msg = EmailMessage()
     msg["Date"] = formatdate()
     msg["From"] = config["from"]
-    msg.set_content(" ".join(sys.argv[1:]))
+    msg.set_content(payload)
     msg = bytes(msg)
 
     with IMAP4_SSL(config["host"]) as mbox:
